35d52f417351a3ecb69e2800990c3a64d0ac52af
SimonBiggs <me@simonbiggs.net>
2021-01-03 12:43:40 +1100

    add iterations to random walk function

--- prototyping/mypy_based_particles.py
+++ prototyping/mypy_based_particles.py
@@ -9,24 +9,25 @@ ParticleKeys = Literal["position", "direction", "energy"]
 Particles = Dict[ParticleKeys, jnp.DeviceArray]
 
 
-@jit
-def a_jitable_pure_function_using_dict(rng_key, particles):
+def random_walk(rng_key, particles: Particles, iterations):
     num_particles = particles["position"].shape[-1]
 
-    random_normal_numbers = random.normal(rng_key, shape=(7, num_particles))
-    (rng_key,) = random.split(rng_key, 1)
+    for _ in range(iterations):
+        random_normal_numbers = random.normal(rng_key, shape=(7, num_particles))
+        (rng_key,) = random.split(rng_key, 1)
 
-    particles["position"] += random_normal_numbers[0:3, :]
-    particles["direction"] += random_normal_numbers[3:6, :]
-    particles["energy"] += random_normal_numbers[7, :]
+        particles["position"] += random_normal_numbers[0:3, :]
+        particles["direction"] += random_normal_numbers[3:6, :]
+        particles["energy"] += random_normal_numbers[7, :]
 
     return rng_key, particles
 
 
-def main():
-    seed = 0
-    num_particles = 1e6
-    rng_key = random.PRNGKey(seed)
+random_walk = jit(random_walk, static_argnums=[2])
+
+
+def particles_zeros(num_particles):
+    num_particles = int(num_particles)
 
     particles: Particles = {
         "position": jnp.zeros((3, num_particles)),
@@ -34,4 +35,19 @@ def main():
         "energy": jnp.zeros((1, num_particles)),
     }
 
-    rng_key, particles = a_jitable_pure_function_using_dict(rng_key, particles)
+    return particles
+
+
+def main():
+    seed = 0
+    rng_key = random.PRNGKey(seed)
+    num_particles = 1e6
+    iterations = 10
+
+    particles = particles_zeros(num_particles)
+
+    rng_key, particles = random_walk(rng_key, particles, iterations)
+
+
+if __name__ == "__main__":
+    main()
