52eddfd86fb47b729197dee8e1eb5c04c902ae1b
darcymason <darcymason@gmail.com>
2021-01-04 14:19:14 -0500

    Restructure

---
+++ egsnrc2py/__init__.py
@@ -0,0 +1,13 @@
+# Copyright 2020 egsnrc2py authors. See LICENSE file for details.
+
+import logging
+import logging.config
+from pathlib import Path
+
+from egsnrc2py._version import __version__, __version_info__
+
+dir_path = Path(__file__).resolve().parent
+logging.config.fileConfig(dir_path / "logging.conf")
+
+# create logger
+logger = logging.getLogger("egsnrc2py")
